1fc14c297fa107215e28ea8c9c5243faa68769cc
Anishka Recherla <68859973+Anishka22@users.noreply.github.com>
2022-07-23 17:14:09 +0530

    Delete backend directory

--- backend/table.sql
+++
@@ -1,1 +0,0 @@
-create table ordermanage( ordernumber int, duedate varchar(255),cusname varchar(255),address varchar(255),phone varchar(255),total int);
--- backend/user.sql
+++
@@ -1,5 +0,0 @@
-create table user(username varchar(200), userpassword varchar(200)); 
-insert into user(username,userpassword) values("ravi","ravi123");
-insert into user(username,userpassword) values("ram","ram123");
-
-insert into ordermanage(ordernumber,duedate,cusname,cusaddress,phone,total) values(1,"1-6-2022","abhi","14-75","1234567890",500);
